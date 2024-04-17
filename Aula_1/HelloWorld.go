1184c6792634d3b8953b7ad7f1fe5fa1fe6839fd
Davi-Pedrosa <163602669+Davi-Pedrosa@users.noreply.github.com>
2024-04-17 16:49:02 -0300

    Add files via upload

--- Aula_1/HelloWorld.go
+++ Aula_1/HelloWorld.go
@@ -2,6 +2,38 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Printf("Hello World!!!")
+func menu() {
+    
+	fmt.Println("Qual operação deseja fazer?")
+
+	fmt.Println(("1) Soma? ")
+	fmt.Println(("2) Subtração? ")
+
+
+}
+
+func main() {
+	var num1, num2, resposta int
+	var soma int = num1 + num2
+	var subtracao int = num1 - num2
+
+	fmt.Println("Bem-vindo(a) a calculadora!")
+
+	fmt.Println("Digite o valor do primeiro número: ")
+	fmt.Scanln(&num2)
+
+	fmt.Println("Digite o valor do segundo número: ")
+	fmt.Scanln(&num2)
+
+	menu()
+	fmt.Scanln(&resposta)
+	switch resposta {
+	case 1:
+		fmt.Println(soma)
+	case 2:
+		fmt.Println(subtracao)
+	default:
+		fmt.Println("Número inválido!")
+	}
+
 }
